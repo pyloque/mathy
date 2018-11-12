7da88094280151bab77168790284aa93814a66a0
qianwp <pyloque@gmail.com>
2018-11-12 19:07:16 +0800

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pyloque/mathy"
+)
+
+func main() {
+	fmt.Println(mathy.Fib(10))
+	fmt.Println(mathy.Fact(10))
+}
---
+++ mathy.go
@@ -0,0 +1,25 @@
+package mathy
+
+func Fib(n int) int64 {
+	if n <= 1 {
+		return 1
+	}
+	var s = make([]int64, n+1)
+	s[0] = 1
+	s[1] = 1
+	for i := 2; i <= n; i++ {
+		s[i] = s[i-1] + s[i-2]
+	}
+	return s[n]
+}
+
+func Fact(n int) int64 {
+	if n <= 1 {
+		return 1
+	}
+	var s int64 = 1
+	for i := 2; i <= n; i++ {
+		s *= int64(i)
+	}
+	return s
+}
